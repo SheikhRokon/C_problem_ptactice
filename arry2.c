5fd44d5ec114b1a94746960b6c6e50e19bd59ab4
SheikhRokon <mdrokon.u545@gmail.com>
2023-09-07 18:08:40 +0600

    arry used input and Max Velu output

---
+++ arry2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a[3],i,max;
+
+    printf("Enter velu :\n");
+    for(i=0; i<3 ; i++){
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0; i<3 ; i++){
+        if(a[i] > max);
+        max = a[i];
+    }
+
+    printf("max velu : %d",max);
+
+return 0 ;
+
+
+}
