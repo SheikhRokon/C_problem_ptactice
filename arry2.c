40a8be0185472a9c3ab18ef149c4e89db9266844
SheikhRokon <mdrokon.u545@gmail.com>
2023-09-08 06:52:32 +0600

    max velu

--- arry2.c
+++ arry2.c
@@ -1,18 +1,14 @@
 #include<stdio.h>
 int main(){
-    int a[3],i,max;
+    int a[3]={20,23,50};
+    int i,max;
 
-    printf("Enter velu :\n");
-    for(i=0; i<3 ; i++){
-        scanf("%d",&a[i]);
-    }
-
-    for(i=0; i<3 ; i++){
+    for(i=0; i>3 ; i++){
         if(a[i] > max);
         max = a[i];
     }
+    printf("%d",max);
 
-    printf("max velu : %d",max);
 
 return 0 ;
 
